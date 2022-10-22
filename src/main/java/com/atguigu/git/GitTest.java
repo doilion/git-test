7d8317feae6af86b5314d669d8fd329c142e11e8
doilion <liwj1193856115@gmail.com>
2022-10-22 09:22:25 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,13 +1,8 @@
 package com.atguigu.git;
 
-/**
- * @Author: lwj
- * @Date: 2022/10/22 - 10 - 22 - 9:13
- * @Description: com.atguigu.git
- * @Version: 1.0
- */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
