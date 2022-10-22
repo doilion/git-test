6aeb01ff276293ca32986b109f1ddb62395fff2a
doilion <liwj1193856115@gmail.com>
2022-10-22 09:18:12 +0800

    初次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: lwj
+ * @Date: 2022/10/22 - 10 - 22 - 9:13
+ * @Description: com.atguigu.git
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
